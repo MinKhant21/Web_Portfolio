5ff5e9630387fd3448018b3e0485190ce57de993
KaungMinKhant <kaungminkhant6889@gmail.com>
2022-07-22 11:51:34 +0630

    webport

---
+++ index.js
@@ -0,0 +1,10 @@
+<script>
+// Progress bars
+$(document).ready(function() {
+$('.progress .progress-bar').css("width",
+    function() {
+        return $(this).attr("aria-valuenow") + "%";
+    }
+)
+});
+</script>
